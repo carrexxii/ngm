a647907255e34e692d9d6b4ee141b970be0f971d
carrexxii <cjbarrett101@gmail.com>
2024-06-12 18:37:50 +0200

    Blade generation

--- ngm/pga.nim
+++ ngm/pga.nim
@@ -1,6 +1,7 @@
 import
     std/[macros, strutils],
     common
+from std/algorithm import product
 
 type
     Real  = float
@@ -15,9 +16,18 @@ func name_of_basis(basis: string): string =
         ("0", "w"),
     )
 
+func blade_abbrev(blade: string): string =
+    case to_lower_ascii blade
+    of "vector"      : result = "vec"
+    of "bivector"    : result = "bivec"
+    of "trivector"   : result = "trivec"
+    of "pseudoscalar": result = "pss"
+    else:
+        assert(false, &"Failed to match '{blade}' to an abbreviation")
+
 macro gen_algebra(comps: varargs[untyped]): untyped =
-    var vecs = nnkConstDef.newTree(
-        ident "full_basis",
+    var full_basis = nnkConstDef.newTree(
+        nnkPostfix.newTree(ident "*", ident "FullBasis"),
         newEmptyNode(),
         newTree nnkBracket,
     )
@@ -43,19 +53,36 @@ macro gen_algebra(comps: varargs[untyped]): untyped =
             func `<=`*(a, b: @@basis): bool {.borrow.}
             func `<` *(a, b: @@basis): bool {.borrow.}
 
-        vecs[2].add (newLit $basis)
+        full_basis[2].add (newLit $basis)
 
-    result.add (nnkConstSection.newTree vecs)
+    result.add (nnkConstSection.newTree full_basis)
     # echo repr result[0]
 
-when defined PGA2D:
-    gen_algebra(e0, e1, e2, e3, e01, e20, e12, e012)
+gen_algebra(e0, e1, e2, e3, e01, e20, e12, e012)
+# when defined PGA2D:
+#     gen_algebra(e0, e1, e2, e3, e01, e20, e12, e012)
+# elif defined PGA3D:
+#     gen_algebra(e0, e1, e2, e3, e01, e02, e03, e12, e31, e23, e021, e013, e032, e123, e0123)
 
 macro gen_blades(blades: varargs[untyped]): untyped =
+    var blade_list: seq[string] # Need this for operator resolution without `AllBlades` defined yet
+    var all_blades = nnkConstDef.newTree(
+        nnkPostfix.newTree(ident "*", ident "AllBlades"),
+        newEmptyNode(),
+        newTree nnkBracket,
+    )
+
     result = newNimNode nnkStmtList
+    let multivec_fields = nnkRecList.newTree(
+        nnkIdentDefs.newTree(
+            nnkPostfix.newTree(ident "*", ident "scalar"),
+            quote do: Real,
+            newEmptyNode()
+        )
+    )
     for (i, blade) in enumerate blades:
         var components = newNimNode nnkRecList
-        for basis in full_basis:
+        for basis in FullBasis:
             if basis.len - 2 != i:
                 continue
 
@@ -78,11 +105,51 @@ macro gen_blades(blades: varargs[untyped]): untyped =
             )
         )
 
+        multivec_fields.add nnkIdentDefs.newTree(
+            nnkPostfix.newTree(ident "*", ident (blade_abbrev $blade)),
+            `blade`,
+            newEmptyNode()
+        )
+
+        blade_list.add $blade
+
+    result.add nnkTypeSection.newTree(
+        nnkTypeDef.newTree(
+            nnkPostFix.newTree(ident "*", ident "Multivector"),
+            newEmptyNode(),
+            nnkObjectTy.newTree(
+                newEmptyNode(),
+                newEmptyNode(),
+                multivec_fields
+            )
+        )
+    )
+
+    var sum_blade : NimNode
+    # var prod_blade: string
+    for b in product [@FullBasis, @FullBasis]:
+        if b[0] == b[1]:
+            continue
+
+        if b[0].len == b[1].len:
+            sum_blade = ident blade_list[b[0].len - 2]
+        else:
+            sum_blade = ident "Multivector"
+
+        echo &"{b} -> {sum_blade}"
+        let T = ident b[0]
+        let U = ident b[1]
+        let ctor = ident (to_lower_ascii $sum_blade)
+        result.add quote("@@") do:
+            template `+`*(a: `@@T`; b: `@@U`): `@@sum_blade` = `@@ctor`(a, b)
+
+    for blade in blade_list:
+        all_blades[2].add (newLit blade)
+    result.add (nnkConstSection.newTree all_blades)
     echo repr result
 
-when defined PGA2D:
-    gen_blades(Vector, Bivector, PseudoScalar)
-
-elif defined PGA3D:
-    algebra(e0, e1, e2, e3, e01, e02, e03, e12, e31, e23, e021, e013, e032, e123, e0123)
-    assert false
+gen_blades(Vector, Bivector, PseudoScalar)
+# when defined PGA2D:
+#     gen_blades(Vector, Bivector, PseudoScalar)
+# elif defined PGA3D:
+#     gen_blades(Vector, Bivector, Trivector, PseudoScalar)
--- tests/pga.nim
+++ tests/pga.nim
@@ -6,6 +6,8 @@ let
     y = 4'e2
     z = 6'e0
 
+echo &"Testing PGA with blades {AllBlades} and basis vectors {FullBasis}"
+
 assert &"{x}, {y}, {z}" == "2.0e1, 4.0e2, 6.0e0"
 assert &"{1'e1}, {2'e2}" == "1.0e1, 2.0e2"
 assert &"{5'e12}" == "5.0e12"
@@ -14,5 +16,5 @@ assert 1'e1 + 1'e1 == 2'e1
 assert 2'e1 + 4'e1 == 6'e1
 assert 3'e2 + 4'e2 == 7'e2
 
-# echo 1'e0 + 1'e1
+echo 1'e0 + 1'e1
 # assert $(1'e0 + 1'e1) == "1'e0 + 1'e1"
