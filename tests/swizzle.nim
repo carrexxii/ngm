8ab93d26bf4b7c22aff275d4ed646cce60d8342b
carrexxii <cjbarrett101@gmail.com>
2024-07-15 16:37:44 +0200

    Add vector binding gen

--- tests/swizzle.nim
+++ tests/swizzle.nim
@@ -1,11 +1,12 @@
 import std/[unittest, strformat], ngm
 
-var v1 = vec(1, 2, 3)
-var v2 = vec(4, 5, 6)
-var v3 = vec(-1, -3, -5)
+var
+    v1 = vec(1, 2, 3)
+    v2 = vec(4, 5, 6)
+    v3 = vec(-1, -3, -5)
 
 echo "===== Swizzle Tests ====="
-echo &"    v1 = {v1};  v2 = {v2}; v3 = {v3}"
+echo &"    v1 = {v1}; v2 = {v2}; v3 = {v3}"
 
 check:
     v1.x == 1
@@ -34,4 +35,10 @@ check:
     v2.zx  == vec(7, 5)
     v3.zzz == vec(1, 1, 1)
     v1.yyz == vec(8, 8, 9)
+
+check:
+    (v1.x + v2.y + v3.z)       == 8
+    (v1.xy + v2.yz + v3.zx)    == vec(8, 18)
+    (v1.xyz + v2.yzx + v3.zxy) == vec(8, 18, 16)
+    (v1.xx + v2.yy + v3.zz)    == vec(8, 8)
 
--- tests/vec.nim
+++ tests/vec.nim
@@ -1,12 +1,25 @@
-import ngm
-from std/strformat import `&`
+import std/[unittest, strformat], ngm
 
 var
     v = Vec3Zero
     u = vec(1, 2, 3)
     w = vec(3, 5, 7)
 
-echo &"v = {v}, u = {u}, w = {w}"
-echo &"{v} + {u} = {v + u}"
-echo &"{w} - {u} = {w - u}"
+echo "===== Vector Tests ====="
+echo &"    v = {v}; u = {u}; w = {w}"
+
+check:
+    v == vec(0, 0, 0)
+    u == vec(1, 2, 3)
+    w == vec(3, 5, 7)
+
+check:
+    (v + w) == vec(3, 5, 7)
+    (w + u) == vec(4, 7, 10)
+    (u + v) == vec(1, 2, 3)
+
+check:
+    (v - w) == vec(-3, -5, -7)
+    (w - u) == vec(2, 3, 4)
+    (u - v) == vec(1, 2, 3)
 
