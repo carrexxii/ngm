df2e716a6c17adfdbd53b7569ad46db0ff5a4212
carrexxii <cjbarrett101@gmail.com>
2024-12-30 19:43:24 +0200

    Update util

--- ngm/borrows.nim
+++ ngm/borrows.nim
@@ -1,25 +1,42 @@
-template borrow_numeric*(T, base: typedesc) =
+import std/typetraits
+
+type BorrowKind* = enum
+    bkAdditive
+    bkMultiplicative
+    bkComparable
+    bkPrintable
+
+template borrow_additive*(T: typedesc) =
+    func `+`*(a: T): T {.borrow.}
     func `-`*(a: T): T {.borrow.}
 
     func `+`*(a, b: T): T {.borrow.}
     func `-`*(a, b: T): T {.borrow.}
-    proc `+=`*(a: var T; b: T) {.borrow.}
-    proc `-=`*(a: var T; b: T) {.borrow.}
 
+    func `+=`*(a: var T; b: T) {.borrow.}
+    func `-=`*(a: var T; b: T) {.borrow.}
+
+template borrow_multiplicative*(T: typedesc; base = distinct_base T) =
     func `*`*(a: T; b: base): T {.borrow.}
-    func `*`*(b: base; a: T): T {.borrow.}
-    proc `*=`*(a: var T; b: base) {.borrow.}
-    when base is SomeFloat:
-        func `/`*(a: T; b: base): T {.borrow.}
-        func `/`*(b: base; a: T): T {.borrow.}
-        proc `/=`*(a: var T; b: base) {.borrow.}
-    else:
-        func `div`*(a: T; b: base): T {.borrow.}
-        func `div`*(b: base; a: T): T {.borrow.}
-        func `mod`*(a: T; b: base): T {.borrow.}
-        func `mod`*(b: base; a: T): T {.borrow.}
+    func `/`*(a: T; b: base): T {.borrow.}
+    func `*`*(a: base; b: T): T {.borrow.}
+    when base is SomeInteger:
+        func `div`*(a: T; b: int): T {.borrow.}
+        func `mod`*(a: T; b: int): T {.borrow.}
 
-    func `<`* (a, b: T): bool {.borrow.}
+template borrow_comparable*(T: typedesc) =
+    func `<`*(a, b: T): bool  {.borrow.}
     func `<=`*(a, b: T): bool {.borrow.}
     func `==`*(a, b: T): bool {.borrow.}
 
+template borrow_printable*(T: typedesc) =
+    func repr*(a: T): string {.borrow.}
+    func `$`*(a: T): string  {.borrow.}
+
+template borrow*(T: typedesc; kinds: varargs[BorrowKind]) =
+    when bkAdditive       in kinds: borrow_additive T
+    when bkMultiplicative in kinds: borrow_multiplicative T
+    when bkComparable     in kinds: borrow_comparable T
+    when bkPrintable      in kinds: borrow_printable T
+
+template borrow_unit*(T: typedesc) = T.borrow bkAdditive, bkMultiplicative, bkComparable, bkPrintable
--- ngm/util.nim
+++ ngm/util.nim
@@ -1,37 +1,54 @@
 import std/math, common, borrows
 from std/strutils import parse_float
 
+func lerp*(a, b, t: Real): Real =
+    a + t*(b - a)
+
+func step*(a: Real; r = (SomeNumber 0)..(SomeNumber 1)): Real =
+    if a < Real r.b:
+        Real r.a
+    else:
+        Real r.b
+
+#[ -------------------------------------------------------------------- ]#
+
 type
     Degrees* = distinct Real
     Radians* = distinct Real
 
-proc `'deg`*(x: string): Degrees {.compileTime.} = Degrees (parse_float x)
-proc `'rad`*(x: string): Radians {.compileTime.} = Radians (parse_float x)
+const π* = Radians Pi
 
-Degrees.borrow_numeric float32
-Radians.borrow_numeric float32
+
+proc `'deg`*(x: string): Degrees {.compileTime.} = Degrees parse_float x
+proc `'°`*(x: string): Degrees   {.compileTime.} = Degrees parse_float x
+proc `'rad`*(x: string): Radians {.compileTime.} = Radians parse_float x
+
+borrow_unit Degrees
+borrow_unit Radians
+
+{.push inline.}
+
+converter `Degrees -> Radians`*(a: Degrees): Radians = Radians a*(Pi / 180)
+converter `Radians -> Degrees`*(a: Radians): Degrees = Degrees a*(180 / Pi)
+
+func `=~`*(a, b: Degrees): bool {.borrow.}
+func `=~`*(a, b: Radians): bool {.borrow.}
+
+func radians*(a: SomeNumber): Radians = Radians a
+func degrees*(a: SomeNumber): Degrees = Degrees a
+func rad*(a: SomeNumber): Radians = radians a
+func deg*(a: SomeNumber): Degrees = degrees a
+
+func radians*(a: Degrees): Radians = `Degrees -> Radians` a
+func degrees*(a: Radians): Degrees = `Radians -> Degrees` a
+func rad*(a: Degrees): Radians = radians a
+func deg*(a: Radians): Degrees = degrees a
 
 func sin*(α: Radians): Real = sin Real α
 func cos*(α: Radians): Real = cos Real α
 func tan*(α: Radians): Real = tan Real α
 
-{.push header: CGLMDir / "util.h".}
-func glm_eq*(a, b: float32): bool    {.importc: "glm_eq" .}
-func glm_rad*(deg: float32): float32 {.importc: "glm_rad".}
-func glm_deg*(rad: float32): float32 {.importc: "glm_deg".}
-
-func glm_lerpc*(`from`, to, t: float32): float32         {.importc: "glm_lerpc"        .}
-func glm_step*(edge, x: float32): float32                {.importc: "glm_step"         .}
-func glm_smooth*(t: float32): float32                    {.importc: "glm_smooth"       .}
-func glm_smoothstep*(edge0, edge1, t: float32): float32  {.importc: "glm_smoothstep"   .}
-func glm_smoothinterpc*(`from`, to, t: float32): float32 {.importc: "glm_smoothinterpc".}
-{.pop.}
-
-{.push inline.}
-
-converter `Degrees -> Radians`*(x: Degrees): Radians = Radians glm_rad (float32 x)
-converter `Radians -> Degrees`*(x: Radians): Degrees = Degrees glm_deg (float32 x)
-
-func lerp*(`from`, to, t: float32): float32 = `from`.glm_lerpc to, t
+func lerp*(a, b: Radians; t: Real): Radians {.borrow.}
+func lerp*(a, b: Degrees; t: Real): Degrees {.borrow.}
 
 {.pop.}
