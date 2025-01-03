47caf9297efd4004f2588bd6744e9ee93b199474
carrexxii <cjbarrett101@gmail.com>
2025-01-03 15:35:26 +0200

    Start updating PGA

--- ngm.nim
+++ ngm.nim
@@ -1,3 +1,6 @@
+if (defined Ngm2D) and (defined Ngm3D):
+    assert false, "Both `Ngm2D` and `Ngm3D` should not be simultaneously defined"
+
 import ngm/[util, vector, matrix, quat, dquat, pga, geometry, interpolation, camera]
 export      util, vector, matrix, quat, dquat, pga, geometry, interpolation, camera
 
--- ngm/pga.nim
+++ ngm/pga.nim
@@ -23,64 +23,155 @@
 # a● = 𝟙 ⟑ ã      (bulk left complement)
 # a○ = 1 ⟇ a̰      (weight left complement)
 
-import common, vector, matrix, borrows
+## 3D Basis: e_1, e_2, e_3, e_0
+##
+## Points  : p, q
+## Planes  : g, h
+## Motors  : Q
+## Flectors: F
+##
+## ∧ = Join
+## ∨ = Meet
+## ★ = Bulk dual
+## ☆ = Weight dual
+##
+## Constructing points:
+## - g ∨ l    (Point where g and l intersect)
+##
+## Constructing lines:
+## - p ∧ q    (line from p to q)
+## - g ∨ h    (line where g and h intersect)
+## - p ∧ g☆   (line through p orthogonal to g)
+##
+## Constructing planes:
+## - l ∧ p    (plane containing l and p)
+## - p ∧ l☆   (plane containing p and orthogonal to l)
+## - l ∧ g☆   (plane containing l and orthogonal to g)
+##
+## Projections:
+## -
+##
+
+import common, util, geometry, vector, matrix
 
 type
-    Scalar* = float32
+    Bivec2D* = Vec3 ## l_xe_{01} + l_ye_{20} + l_we_{12}
+    Bivec3D* = object
+        v*: Vec3 ## v_xe_{01} + v_ye_{02} + v_ze_{03}
+        m*: Vec3 ## m_xe_{23} + m_ye_{31} + m_ze_{12}
 
-    Vec* = object
-        x*, y*, z*, w*: Scalar
+    Trivec3D* = Vec4 ## g_xe_{023} + g_ye_{031} + g_ze_{012} + g_we_{321}
 
-    Bivec* = object
-        wx*, wy*, wz*: Scalar
-        yz*, zx*, xy*: Scalar
+    Antiscalar2D* = Real # e_{012}
+    Antiscalar3D* = Real # e_{0123}
 
-    Trivec* = object
-        wyz*, wzx*, wxy*, zyx*: Scalar
+    Motor* = object
+        v*: Vec4
+        m*: Vec4
 
-    Antiscalar* = distinct Scalar
-
-    AnyGrade = Scalar | Vec | Bivec | Trivec | Antiscalar
+    Flector* = object
+        p*: Vec4
+        g*: Vec4
 
 {.push inline.}
 
-Antiscalar.borrow_numeric Scalar
+func bivec*(x, y, w: Real): Bivec2D                = Bivec2D [x, y, w]
+func bivec*(vx, vy, vz, mx, my, mz: Real): Bivec3D = Bivec3D(v: [vx, vy, vz], m: [mx, my, mz])
+func trivec*(x, y, z, w: Real): Trivec3D           = Trivec3D [x, y, z, w]
 
-func vec*(x, y, z: Scalar; w = Scalar 1)   : Vec    = Vec(x: x, y: y, z: z, w: w)
-func bivec*(wx, wy, wz, yz, zx, xy: Scalar): Bivec  = Bivec(wx: wx, wy: wy, wz: wz, yz: yz, zx: zx, xy: xy)
-func trivec*(wyz, wzx, wxy, zyx: Scalar)   : Trivec = Trivec(wyz: wyz, wzx: wzx, wxy: wxy, zyx: zyx)
+when defined Ngm2D:
+    type
+        Bivec*      = Bivec2
+        Antiscalar* = Antiscalar2
+elif defined Ngm3D:
+    type
+        Bivec*      = Bivec3D
+        Trivec*     = Trivec3D
+        Antiscalar* = Antiscalar3D
 
-func `$`*(s: Scalar)    : string = &"Scalar {float s:.2f}"
-func `$`*(v: Vec)       : string = &"Vector [{v.x:.2f}, {v.y:.2f}, {v.z:.2f}, {v.w:.2f}]"
-func `$`*(b: Bivec)     : string = &"Bivector [{b.wx:.2f}, {b.wy:.2f}, {b.wz:.2f}; {b.yz:.2f}, {b.zx:.2f}, {b.xy:.2f}]"
-func `$`*(t: Trivec)    : string = &"Trivector [{t.wyz:.2f}, {t.wzx:.2f}, {t.wxy:.2f}, {t.zyx:.2f}]"
-func `$`*(a: Antiscalar): string = &"Antiscalar {float a:.2f}"
+func wedge*(p, q: Vec3): Bivec2D =
+    ## \displaylines{\begin{flalign}
+    ## p \wedge q &= (p_xe_1 + p_ye_2 + p_we_0) \wedge (q_xe_1 + q_ye_2 + q_we_0) \\
+    ##            &= p_xq_xe_1e_1 + p_xq_ye_1e_2 + p_xq_we_1e_0 + p_yq_xe_2e_1 + p_yq_ye_2e_2 +
+    ##               p_yq_we_2e_0 + p_wq_xe_0e_1 + p_wq_ye_0e_2 + p_wq_we_0e_0 \\
+    ##            &= p_xq_ye_{12} + p_xq_we_{10} + p_yq_xe_{21} + p_yq_we_{20} + p_wq_xe_{01} + p_wq_ye_{02} \\
+    ##            &= (p_wq_x - p_xq_w)e_{01} + (p_yq_w - p_wq_y)e_{20} + (p_xq_y - p_yq_x)e_{12}
+    ## \end{flalign}}
+    Bivec2D [p.w*q.x - p.x*q.w,
+             p.y*q.w - p.w*q.y,
+             p.x*q.y - p.y*q.x]
 
-func grade*(s: Scalar)    : int = 0
-func grade*(v: Vec)       : int = 1
-func grade*(b: Bivec)     : int = 2
-func grade*(t: Trivec)    : int = 3
-func grade*(a: Antiscalar): int = 4
+func wedge*(p, q: Point2D): Bivec2D =
+    ## l = p ∧ q
+    Bivec2D [q.x - p.x,
+             p.y - q.y,
+             p.x*q.y - p.y*q.x]
 
-func antigrade*(s: Scalar)    : int = 4
-func antigrade*(v: Vec)       : int = 3
-func antigrade*(b: Bivec)     : int = 2
-func antigrade*(t: Trivec)    : int = 1
-func antigrade*(a: Antiscalar): int = 0
+func wedge*(p, q: Vec4): Bivec3D =
+    ## \displaylines{\begin{flalign}
+    ## p \wedge q &= (p_xe_1 + p_ye_2 + p_ze_3 + p_we_0) \wedge (q_xe_1 + q_ye_2 + q_ze_3 + q_we_0) \\
+    ##            &= p_xe_1q_xe_1 + p_xe_1q_ye_2 + p_xe_1q_ze_3 + p_xe_1q_we_0 +
+    ##               p_ye_2q_xe_1 + p_ye_2q_ye_2 + p_ye_2q_ze_3 + p_ye_2q_we_0 +
+    ##               p_ze_3q_xe_1 + p_ze_3q_ye_2 + p_ze_3q_ze_3 + p_ze_3q_we_0 +
+    ##               p_we_0q_xe_1 + p_we_0q_ye_2 + p_we_0q_ze_3 + p_we_0q_we_0 \\
+    ##            &= p_xq_x + p_xq_ye_{12} + p_xq_ze_{13} + p_xq_we_{10} +
+    ##               p_yq_xe_{21} + p_yq_y + p_yq_ze_{23} + p_yq_we_{20} +
+    ##               p_zq_xe_{31} + p_zq_ye_{32} + p_zq_z + p_zq_we_{30} +
+    ##               p_wq_xe_{01} + p_wq_ye_{02} + p_wq_ze_{03} \\
+    ##            &= (p_yq_z - p_zq_y)e_{23} + (p_zq_x - p_xq_z)e_{31} + (p_xq_y - p_yq_x)e_{12} +
+    ##               (p_wq_x - p_xq_w)e_{01} + (p_wq_y - p_yq_w)e_{02} + (p_wq_z - p_zq_w)e_{03} +
+    ##               p_xq_x + p_yq_y + p_zq_z
+    ## \end{flalign}}
+    Bivec3D(v: [p.w*q.x - p.x*q.w,
+                p.w*q.y - p.y*q.w,
+                p.w*q.z - p.z*q.w],
+            m: [p.y*q.z - p.z*q.y,
+                p.z*q.x - p.x*q.z,
+                p.x*q.y - p.y*q.x])
 
-func bulk*(s: Scalar)    : Scalar = s
-# func bulk*(v: Vec)       : Vec3   = vec(v.x, v.y, v.z)
-# func bulk*(b: Bivec)     : Vec3   = vec(b.yz, b.zx, b.xy)
-func bulk*(t: Trivec)    : Scalar = t.zyx
-func bulk*(a: Antiscalar): Scalar = Scalar 0
+func wedge*(p, q: Point3D): Bivec3D =
+    ## l = p ∧ q
+    Bivec3D(v: [q.x - p.x,
+                q.y - p.y,
+                q.z - p.z],
+            m: [p.y*q.z - p.z*q.y,
+                p.z*q.x - p.x*q.z,
+                p.x*q.y - p.y*q.x])
 
-func weight*(s: Scalar)    : Scalar     = Scalar 0
-func weight*(v: Vec)       : Scalar     = v.w
-# func weight*(b: Bivec)     : Vec3       = vec(b.wx, b.wy, b.wz)
-# func weight*(t: Trivec)    : Vec3       = vec(t.wyz, t.wzx, t.wxy)
-func weight*(a: Antiscalar): Antiscalar = a
+func wedge*(l: Bivec3D; p: Vec4): Trivec3D =
+    ## g = l ∧ p
+    Trivec3D [l.v.y*p.z - l.v.z*p.y + l.m.x*p.w,
+              l.v.z*p.x - l.v.x*p.z + l.m.y*p.w,
+              l.v.x*p.y - l.v.y*p.x + l.m.z*p.w,
+             -l.m.x*p.x - l.m.y*p.y + l.m.z*p.z]
 
-func `●`*(x: AnyGrade): auto = x.bulk
-func `○`*(x: AnyGrade): auto = x.weight
+func wedge*(l: Bivec3D; p: Point3D): Trivec3D =
+    ## g = l ∧ p
+    Trivec3D [l.v.y*p.z - l.v.z*p.y + l.m.x,
+              l.v.z*p.x - l.v.x*p.z + l.m.y,
+              l.v.x*p.y - l.v.y*p.x + l.m.z,
+             -l.m.x*p.x - l.m.y*p.y + l.m.z*p.z]
 
-{.pop.} # inline
+func antiwedge*(g, h: Trivec3D): Bivec3D =
+    ## l = g ∨ h
+    Bivec3D(v: [g.z*h.y - g.y*h.z,
+                g.x*h.z - g.z*h.x,
+                g.y*h.x - g.x*h.y],
+            m: [g.x*h.w - g.w*h.x,
+                g.y*h.w - g.w*h.y,
+                g.z*h.w - g.w*h.z])
+
+func antiwedge*(g: Trivec3D; l: Bivec3D): Vec4 =
+    ## p = g ∨ l
+    [g.z*l.m.y - g.y*l.m.z + g.w*l.v.x,
+     g.x*l.m.z - g.z*l.m.x + g.w*l.v.y,
+     g.y*l.m.x - g.x*l.m.y + g.w*l.v.z,
+    -g.x*l.v.x - g.y*l.v.y - g.z*l.v.z]
+
+func `∧`*(p, q: Vec3 | Point2D): Bivec2D = wedge p, q
+func `∧`*(p, q: Vec4 | Point3D): Bivec3D = wedge p, q
+
+func `∨`*(g, h: Trivec3D): Bivec3D       = antiwedge g, h
+func `∨`*(g: Trivec3D; l: Bivec3D): Vec4 = antiwedge g, l
+
+{.pop.}
