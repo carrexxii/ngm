50573ec44992c24aa117bbd64ba8ebf46a9eeb2b
carrexxii <cjbarrett101@gmail.com>
2025-01-04 18:15:33 +0200

    .

--- ngm.nim
+++
@@ -1,8 +0,0 @@
-if (defined Ngm2D) and (defined Ngm3D):
-    assert false, "Both `Ngm2D` and `Ngm3D` should not be simultaneously defined"
-
-import ngm/[util, vector, matrix, quat, dquat, pga, geometry, interpolation, camera]
-export      util, vector, matrix, quat, dquat, pga, geometry, interpolation, camera
-
-import ngm/common
-export common.`=~`
